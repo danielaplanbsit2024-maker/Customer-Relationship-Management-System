ab3538dc36b18e762a1884f76f26a43548615c3e
DanielApln <271739283+danielaplanbsit2024-maker@users.noreply.github.com>
2026-03-28 18:56:07 +0800

    Add project files.

---
+++ Customer Relationship Management/Form1.cs
@@ -0,0 +1,10 @@
+namespace Customer_Relationship_Management
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
